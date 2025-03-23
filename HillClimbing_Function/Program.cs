30b387b9c26f009d3609b23b8a2034af8bdd8507
Márton Szőllős <marci.szollos@gmail.com>
2025-03-23 16:18:52 +0100

    Add project files.

---
+++ HillClimbing_Function/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HillClimbing_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
